b8bd2f21d4d22eec858f5ea7a2c4c94420b3073a
Martinalbertengo <tincho9704@gmail.com>
2023-03-01 21:30:17 -0300

    first commit

---
+++ src/components/ContentRowMovies.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import SmallCard from './SmallCard';
+
+/*  Cada set de datos es un objeto literal */
+
+/* <!-- Movies in DB --> */
+
+
+function ContentRowMovies(props){
+    return (
+    
+        <div className="row">
+            
+            {props.cantidades.map( ({nombre, cantidad}, i) => {
+                return <SmallCard cantidades={{nombre, cantidad}} key={i}/>
+            
+            })}
+
+        </div>
+    )
+}
+
+export default ContentRowMovies;
